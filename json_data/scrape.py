1995d149847abcf536b839b2859f2f2864674be7
Banknn <bank.tr2004@gmail.com>
2024-12-07 23:48:12 +0700

    fix script

--- json_data/scrape.py
+++ json_data/scrape.py
@@ -7,6 +7,7 @@ year_list = ['2018', '2019', '2020', '2021', '2022', '2023']
 informations = []
 
 for year in year_list:
+    #path of raw data
     folder_path = f'C:/CEDT/Semester_3/2110403/project_data/{year}'
     file_list = glob.glob(f'{folder_path}/*')
     file_list = [os.path.normpath(file_path) for file_path in file_list]
@@ -19,7 +20,6 @@ for year in year_list:
                 coredata = data['abstracts-retrieval-response']['coredata']
 
                 info = {
-                    "File": os.path.basename(file_path),
                     "Title": coredata.get("dc:title", ""),
                     "Authors": ", ".join([author.get("ce:indexed-name", "") for author in coredata.get("dc:creator", {}).get("author", [])]),
                     "Publication Name": coredata.get("prism:publicationName", ""),
@@ -38,6 +38,7 @@ for year in year_list:
 
 if informations:
     title = pd.DataFrame(informations)
+    #path of exported file
     output_path2 = 'C:/CEDT/Semester_3/2110403/project_data/information.csv'
     title.to_csv(output_path2, index=False, encoding='utf-8')
     print(f'Information data successfully exported to {output_path2}')
